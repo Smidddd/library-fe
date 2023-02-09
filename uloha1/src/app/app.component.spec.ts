43ce72a85b6807e3d93449fd289dc232041af8df
Erik <erikkulka131@gmail.com>
2023-02-09 20:49:58 +0100

    uloha1

--- uloha1/src/app/app.component.spec.ts
+++ uloha1/src/app/app.component.spec.ts
@@ -16,16 +16,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'uloha1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('uloha1');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('uloha1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test2 app is running!');
   });
 });
--- uloha1/src/app/app.component.ts
+++ uloha1/src/app/app.component.ts
@@ -5,6 +5,52 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'uloha1';
+  cislo = "0"
+  changeCheckbox(cislo: String): void{
+    var num = new Number(cislo);
+
+    var bin = num.toString(2);
+
+    let reversepola: string = "";
+
+    for (let char of bin){
+      reversepola = char + reversepola
+    }
+
+    for(let i=0;i<=7;i++){
+        let checkbox = document.getElementById(
+          i.toString()
+        ) as HTMLInputElement | null;
+
+            if (checkbox != null) {
+              if (reversepola[i] == '1'){
+                  checkbox.checked = true;
+                }else {
+                  checkbox.checked = false;
+                }
+              }
+        }
+  }
+
+  changeInputNumber(): void {
+    let polecisla = "";
+    for (let i = 7; i >= 0; i--) {
+      let checkbox = document.getElementById(
+        i.toString()
+      ) as HTMLInputElement | null;
+
+      if (checkbox != null) {
+        if (checkbox.checked == true) {
+          polecisla = polecisla.concat("1")
+        } else {
+          polecisla = polecisla.concat("0")
+        }
+      }
+    }
+    let x = parseInt(polecisla, 2);
+    this.cislo = x.toString();
+  }
 }
+
