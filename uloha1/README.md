3470351715c102c75e5da4239515f5bc452826b7
majo <mariochrusc@gmail.com>
2023-02-15 14:54:08 +0100

    Uloha2

--- uloha1/README.md
+++ uloha1/README.md
@@ -1,6 +1,6 @@
-# Uloha1
+# ChruscUloha1
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.5.
 
 ## Development server
 
--- uloha1/src/app/app.component.ts
+++ uloha1/src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+export enum Menu {
+  BOOKS = 'BOOKS',
+  USERS = 'USERS',
+  BORROWINGS = 'BORROWINGS',
+
+  GENRE = 'GENRES'
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
+
 export class AppComponent {
-  title = 'uloha1';
+    menu = Menu;
+  actualMenu: Menu = Menu.USERS;
+  form: FormGroup;
+
+  formBooks: FormGroup;
+  formGenres: FormGroup;
+  formBorrows: FormGroup;
+  persons: Array<any> = [];
+  books: Array<any> = [];
+  genres: Array<any> = [];
+  borrows: Array<any> = [];
+
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(),
+      surname: new FormControl()
+    })
+    this.formBooks = new FormGroup({
+      autor: new FormControl(),
+      nazovKnihy: new FormControl()
+    })
+    this.formGenres = new FormGroup({
+      zaner: new FormControl()
+    })
+    this.formBorrows = new FormGroup({
+      name: new FormControl(),
+      surname: new FormControl(),
+      nazovKnihy: new FormControl()
+    })
+  }
+  changeMenu(menuItem: Menu): void {
+    this.actualMenu = menuItem;
+  }
+
+  savePerson(): void {
+    this.persons.push(
+      this.form.value);
+    this.form.reset();
+  }
+  saveBooks(): void {
+    this.books.push(
+      this.formBooks.value);
+    this.formBooks.reset();
+  }
+  saveBorrows(): void {
+    this.borrows.push(
+      this.formBorrows.value);
+    this.formBorrows.reset();
+  }
+  saveGenres(): void {
+    this.genres.push(
+      this.formGenres.value);
+    this.formGenres.reset();
+  }
+
 }
+
+
+
+
+
+
+
+
